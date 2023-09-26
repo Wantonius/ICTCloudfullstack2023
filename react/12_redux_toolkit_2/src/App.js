fcf703f10a526e8e893bb4e767021695e961e572
Erno Hentonen <erno.hentonen@opiframe.com>
2023-09-26 10:21:55 +0300

    redux toolkit

---
+++ react/12_redux_toolkit_2/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route,Navigate} from 'react-router-dom';
+import {useEffect} from 'react';
+import ShoppingForm from './components/ShoppingForm';
+import ShoppingList from './components/ShoppingList';
+import Navbar from './components/Navbar';
+import {useDispatch} from 'react-redux';
+import {getList} from './store/shoppingSlice'
+
+function App() {
+	
+	const dispatch = useDispatch();
+	
+	useEffect(() => {
+		dispatch(getList())
+	},[])
+	
+	return (
+		<div className="App">
+			<Navbar/>
+			<Routes>
+				<Route path="/" element={<ShoppingList />}/>
+				<Route path="/form" element={<ShoppingForm />}/>
+				<Route path="*" element={<Navigate to="/"/>}/>
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
